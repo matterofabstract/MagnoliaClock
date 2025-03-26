5dab5a0635d3155fa35bc4f5d50c197a7f4f0eda
Benjamin Keating <ben@abstractly.io>
2025-03-26 11:20:48 -0500

    Embedded SVG to remove file dep

--- src/lib/Portrait.svelte
+++ src/lib/Portrait.svelte
@@ -40,43 +40,68 @@
     </div>
   </div>
 
-  <div class="bg-[url('/PortraitBg.svg')] bg-fit bg-center bg-no-repeat w-full h-[194px] flex flex-col items-start justify-start pt-5.5">
+  <div class="relative w-full h-[194px] flex flex-col items-start justify-start pt-5.5">
 
-  <div class="flex justify-center items-start w-full h-[83px]">
-
-    <div class="flex italic font-semibold flex-col justify-start py-2 gap-[9px] h-full items-end pr-2 text-[#2c2c2c]" style="font-family:Arial, Helvetica, sans-serif;">
-      <div class="text-[13px]">AM</div>
-      <div class="text-[11px]">WAKE 1</div>
+    <!-- Panel background -->
+    <div class="absolute top-0 left-0 w-full h-full z-0">
+      <svg width="100%" height="100%" viewBox="0 0 376 186" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+          <defs>
+              <linearGradient x1="50%" y1="62.3008%" x2="37.8677083%" y2="40.0226129%" id="linearGradient-1">
+                  <stop stop-color="#110E0Cad" offset="0%"></stop>
+                  <stop stop-color="#1C1C1Cad" offset="100%"></stop>
+              </linearGradient>
+              <radialGradient cx="71.4365858%" cy="10.2234543%" fx="71.4365858%" fy="10.2234543%" r="170.673689%" gradientTransform="translate(0.714366,0.102235),scale(0.494681,1.000000),rotate(72.453980),scale(1.000000,3.317522),translate(-0.714366,-0.102235)" id="radialGradient-2">
+                  <stop stop-color="#FFFFFFad" stop-opacity="0.934495192" offset="0%"></stop>
+                  <stop stop-color="#000000ad" offset="100%"></stop>
+              </radialGradient>
+          </defs>
+          <g id="GE-Alarm-Block-(iPhone-App)" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+              <g id="Home" transform="translate(0.000000, -239.000000)" fill-rule="nonzero">
+                  <g id="Silver-Frame" transform="translate(0.000000, 239.000000)">
+                      <rect id="Rectangle-2-Copy-3" fill="url(#linearGradient-1)" x="0" y="0" width="375" height="186"></rect>
+                      <path d="M376,0 L376,186 L0,186 L0,0 L376,0 Z M350.503882,5.33971292 L25.4955804,5.33971292 C18.5386329,5.33971292 12.8147354,10.8167289 12.5082054,17.7669201 L12.5082054,17.7669201 L5.92259801,167.087494 C5.91418234,167.278309 5.90997297,167.469287 5.90997297,167.660287 C5.90997297,174.839989 11.7302712,180.660287 18.909973,180.660287 L18.909973,180.660287 L357.08949,180.660287 C357.28049,180.660287 357.471468,180.656078 357.662283,180.647662 C364.835012,180.331317 370.39321,174.260223 370.076865,167.087494 L370.076865,167.087494 L363.491257,17.7669201 C363.184727,10.8167289 357.46083,5.33971292 350.503882,5.33971292 L350.503882,5.33971292 Z" id="Combined-Shape" fill="url(#radialGradient-2)"></path>
+                  </g>
+              </g>
+          </g>
+      </svg>
     </div>
 
-    <div class="flex flex-col flex-grow max-w-[220px]">
-      <div class="bg-[#0f0506ad] flex-1 flex items-center justify-between px-3 rounded-lg">
-        <div class="flex items-center justify-between flex-col h-full min-h-[35px]">
-          <div class="w-2 h-2 rounded-full" style="background-color: {new Date().getHours() >= 12 ? '#1d080b' : '#c90000'};"></div>
-          <div class="w-2 h-2 bg-[#1d080b] rounded-full"></div>
-        </div>
+    <!-- Main Display -->
+    <div class="flex justify-center items-start w-full h-[83px]">
 
-        <div class="relative flex flex-col items-end justify-center text-4xl mx-2 font-digital font-bold whitespace-nowrap min-h-[60px]">
-          <div class="text-[#21080c]">00 00</div>
-          <div class="absolute text-[#ff0000]" style="text-shadow: 0 0 10px #ff000021;">{time}</div>
+      <div class="flex italic font-semibold flex-col justify-start py-2 gap-[9px] h-full items-end pr-2 text-[#2c2c2c]" style="font-family:Arial, Helvetica, sans-serif;">
+        <div class="text-[13px]">AM</div>
+        <div class="text-[11px]">WAKE 1</div>
+      </div>
+
+      <div class="flex flex-col flex-grow max-w-[220px]">
+        <div class="bg-[#0f0506ad] flex-1 flex items-center justify-between px-3 rounded-lg">
+          <div class="flex items-center justify-between flex-col h-full min-h-[35px]">
+            <div class="w-2 h-2 rounded-full" style="background-color: {new Date().getHours() >= 12 ? '#1d080b' : '#c90000'};"></div>
+            <div class="w-2 h-2 bg-[#1d080b] rounded-full"></div>
+          </div>
+
+          <div class="relative flex flex-col items-end justify-center text-4xl mx-2 font-digital font-bold whitespace-nowrap min-h-[60px]">
+            <div class="text-[#21080c]">00 00</div>
+            <div class="absolute text-[#ff0000]" style="text-shadow: 0 0 10px #ff000021;">{time}</div>
+          </div>
+          <div class="flex items-center justify-end flex-col h-full min-h-[35px]">
+            <div class="w-2 h-2 bg-[#1d080b] rounded-full"></div>
+          </div>
         </div>
-        <div class="flex items-center justify-end flex-col h-full min-h-[35px]">
-          <div class="w-2 h-2 bg-[#1d080b] rounded-full"></div>
+        <div class="uppercase text text-[#2c2c2c] opacity-80 font-semibold">
+          <div class="pt-1 pl-5 text-[10px]">Battery Backup</div>
         </div>
       </div>
-      <div class="uppercase text text-[#2c2c2c] opacity-80 font-semibold">
-        <div class="pt-1 pl-5 text-[10px]">Battery Backup</div>
+
+      <div class="flex italic font-semibold flex-col items-end justify-start h-full pl-4 pb-2 text-[#2c2c2c]" style="font-family:Arial, Helvetica, sans-serif;">
+        <div class="relative top-[-8px]">
+          <div class="bg-[url('/GELogoMark.png')] bg-no-repeat w-6 h-6 opacity-20 relative bg-cover"></div>
+        </div>
+        <div class="text-[11px] relative top-[11px]">WAKE 2</div>
       </div>
+
     </div>
-
-    <div class="flex italic font-semibold flex-col items-end justify-start h-full pl-4 pb-2 text-[#2c2c2c]" style="font-family:Arial, Helvetica, sans-serif;">
-      <div class="relative top-[-8px]">
-        <div class="bg-[url('/GELogoMark.png')] bg-no-repeat w-6 h-6 opacity-20 relative bg-cover"></div>
-      </div>
-      <div class="text-[11px] relative top-[11px]">WAKE 2</div>
-    </div>
-
-  </div>
 
   <!-- Radio Band -->
    <div class="max-w-[calc(100%-50px)] w-full mx-auto mt-0.5">
